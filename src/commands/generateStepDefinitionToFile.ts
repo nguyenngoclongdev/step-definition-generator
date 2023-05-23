590e9a8e8d136ab45628d7ce0fbdf34fe2cc3524
LongNguyen <nguyenngoclong@live.com>
2023-05-23 08:09:46 +0700

    fix: improve the source code

--- src/commands/generateStepDefinitionToFile.ts
+++ src/commands/generateStepDefinitionToFile.ts
@@ -6,28 +6,6 @@ import { ExtensionConfiguration } from '../extension';
 import { getFeatureFilePath, getLanguage, getLanguageExt, getRunner, showErrorMessageWithDetail } from '../utils';
 import path = require('path');
 
-const overrideFile = (stepDefinitionFilePath: string, content: string): void => {
-    if (!content.trim()) {
-        return;
-    }
-
-    const dirPath = path.dirname(stepDefinitionFilePath);
-    fs.mkdir(dirPath, () => {
-        fs.writeFileSync(stepDefinitionFilePath, content);
-    });
-};
-
-const appendFile = (stepDefinitionFilePath: string, content: string): void => {
-    if (!content.trim()) {
-        return;
-    }
-
-    const dirPath = path.dirname(stepDefinitionFilePath);
-    fs.mkdir(dirPath, () => {
-        fs.appendFileSync(stepDefinitionFilePath, content);
-    });
-};
-
 export const generateStepDefinitionToFile = async (uri: vscode.Uri, config: ExtensionConfiguration): Promise<void> => {
     try {
         const runner = getRunner(config.runner);
@@ -48,16 +26,33 @@ export const generateStepDefinitionToFile = async (uri: vscode.Uri, config: Exte
         // Get feature content
         const featureFileContent = readFileSync(featureFilePath, 'utf8');
 
-        // Generate code and write to file
+        // Generate code
+        let stepDefinitionOutput = "";
         const gherkinCodeParse = new GherkinCodeParse(runner, language);
-        if (fs.existsSync(stepDefinitionFilePath)) {
+        const isRegenerateStepDefinition = fs.existsSync(stepDefinitionFilePath);
+        if (isRegenerateStepDefinition) {
             const stepDefinitionFileContent = fs.readFileSync(stepDefinitionFilePath, 'utf-8');
-            const output = gherkinCodeParse.parse(featureFileContent, stepDefinitionFileContent);
-            appendFile(stepDefinitionFilePath, output);
+            stepDefinitionOutput = gherkinCodeParse.parse(featureFileContent, stepDefinitionFileContent);
         } else {
-            const output = gherkinCodeParse.parse(featureFileContent);
-            overrideFile(stepDefinitionFilePath, output);
+            stepDefinitionOutput = gherkinCodeParse.parse(featureFileContent);
         }
+
+        // Check the output content
+        const isEmptyStepDefinitionOutput = stepDefinitionOutput.trim() === '';
+        if (isEmptyStepDefinitionOutput) {
+            vscode.window.showInformationMessage('No changes detected in the content of the feature file!');
+            return;
+        }
+
+        // Write output to file
+        const dirPath = path.dirname(stepDefinitionFilePath);
+        fs.mkdir(dirPath, () => {
+            if (isRegenerateStepDefinition) {
+                fs.appendFileSync(stepDefinitionFilePath, stepDefinitionOutput);
+            } else {
+                fs.writeFileSync(stepDefinitionFilePath, stepDefinitionOutput);
+            }
+        });
 
         // Show message
         vscode.window.showInformationMessage('Step definitions generated successfully!', 'View Output Path')
